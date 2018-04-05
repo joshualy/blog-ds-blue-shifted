78a833e0ecd45a00b1d0180aa4646c3f4241cbbf
Joshua Lytle <joshualyte@gmail.com>
2018-04-05 15:19:46 -0600

    Github and resumes open in separate windows. Added Research link to menu item personal.

--- personal_dj/urls.py
+++ personal_dj/urls.py
@@ -20,32 +20,10 @@ from django.conf import settings
 
 from appHome import views
 
-# blog_urls = ([
-#     url(r'^', include('zinnia.urls.capabilities')),
-#     url(r'^search/', include('zinnia.urls.search')),
-#     url(r'^sitemap/', include('zinnia.urls.sitemap')),
-#     url(r'^trackback/', include('zinnia.urls.trackback')),
-#     url(r'^blog/tags/', include('zinnia.urls.tags')),
-#     url(r'^blog/feeds/', include('zinnia.urls.feeds')),
-#     url(r'^blog/random/', include('zinnia.urls.random')),
-#     url(r'^blog/authors/', include('zinnia.urls.authors')),
-#     url(r'^blog/categories/', include('zinnia.urls.categories')),
-#     url(r'^blog/comments/', include('zinnia.urls.comments')),
-#     url(r'^blog/', include('zinnia.urls.entries')),
-#     url(r'^blog/', include('zinnia.urls.archives')),
-#     url(r'^blog/', include('zinnia.urls.shortlink')),
-#     url(r'^blog/', include('zinnia.urls.quick_entry'))
-# ], 'zinnia')
-
-
 
 
 urlpatterns = [
     url(r'^ds-blue-shifted/',include('appHome.urls', namespace="appHome")),
     url(r'^$',views.index,name='index'),
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('simpleblog.urls')),
-    # url(r'^weblog/', include('zinnia.urls')),
-    # url(r'^comments/', include('django_comments.urls')),
-    # url(r'^', include(blog_urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
