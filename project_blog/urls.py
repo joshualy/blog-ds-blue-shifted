a7b5734670f034f7225937508b4a14aafb17bf6e
Joshua Lytle <joshualyte@gmail.com>
2018-04-06 19:33:55 -0600

    Renamed project to project_blog

--- project_blog/urls.py
+++ project_blog/urls.py
@@ -1,4 +1,4 @@
-"""personal_dj URL Configuration
+"""project_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
